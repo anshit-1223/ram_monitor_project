4c54b12e21ab3bc3583cc51d91b1eb185f21947f
Anshit Verma <anshit@kali>
2025-05-20 18:52:36 +0530

    Email User Conf Added

--- ram_status.sh
+++ ram_status.sh
@@ -3,11 +3,12 @@
 #!/bin/bash
 
 source ./.ram_threshold.conf
+source ./.email_user.conf
 FREE_SPACE=$( free -mt | grep "Total" | awk '{print $4}' )
 
 if [[ $FREE_SPACE -lt THRESHOLD ]]
 then
-    echo "WARNING! RAM is running low." | mail -s "Regarding RAM Usage" anshit1223@gmail.com
+    echo "WARNING! RAM is running low." | mail -s "Regarding RAM Usage" Email
 else
     echo "RAM is Sufficient - $FREE_SPACE M"
 fi
