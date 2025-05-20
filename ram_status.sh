4358aa53c28803aae97dad9d5753bbdaebcc3b37
Anshit Verma <anshit@kali>
2025-05-20 18:08:36 +0530

    Email Notification Added

--- ram_status.sh
+++ ram_status.sh
@@ -7,7 +7,7 @@ TH=400
 
 if [[ $FREE_SPACE -lt $TH ]]
 then
-    echo "WARNING! RAM is running low."
+    echo "WARNING! RAM is running low." | mail -s "Regarding RAM Usage" anshit1223@gmail.com
 else
     echo "RAM is Sufficient - $FREE_SPACE M"
 fi
